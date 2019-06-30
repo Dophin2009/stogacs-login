48a9d415eab92b72618ef36da7f912a4457e373c
Dophin2009 <21zhaoe@gmail.com>
2019-06-30 15:51:07 -0400

    Move ModelMapper bean configuration to separate class

--- src/main/java/net/edt/Application.java
+++ src/main/java/net/edt/Application.java
@@ -1,41 +1,14 @@
 package net.edt;
 
-import org.modelmapper.Converter;
-import org.modelmapper.ModelMapper;
-import org.modelmapper.Provider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
-    }
-
-    @Bean
-    public ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
-
-        Provider<LocalDate> localDateProvider = request -> LocalDate.now();
-        Converter<String, LocalDate> stringLocalDateConverter = ctx -> {
-            String source = ctx.getSource();
-            DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-            return source == null ? null : LocalDate.parse(source, format);
-        };
-
-        Converter<LocalDate, String> localDateStringConverter = ctx -> ctx.getSource().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-
-        modelMapper.createTypeMap(String.class, LocalDate.class).setProvider(localDateProvider);
-        modelMapper.addConverter(stringLocalDateConverter, String.class, LocalDate.class);
-        modelMapper.addConverter(localDateStringConverter, LocalDate.class, String.class);
-
-        return modelMapper;
     }
 
 }
---
+++ src/main/java/net/edt/configuration/beans/ModelMapperBean.java
@@ -0,0 +1,54 @@
+package net.edt.configuration.beans;
+
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.Provider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class ModelMapperBean {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+
+        // Provider and converters for LocalDate
+        Provider<LocalDate> localDateProvider = request -> LocalDate.now();
+        Converter<String, LocalDate> stringLocalDateConverter = ctx -> {
+            String source = ctx.getSource();
+            DateTimeFormatter format = DateTimeFormatter.ISO_DATE;
+            return source == null ? null : LocalDate.parse(source, format);
+        };
+
+        Converter<LocalDate, String> localDateStringConverter = ctx -> ctx.getSource()
+                .format(DateTimeFormatter.ISO_DATE);
+
+        modelMapper.createTypeMap(String.class, LocalDate.class).setProvider(localDateProvider);
+        modelMapper.addConverter(stringLocalDateConverter, String.class, LocalDate.class);
+        modelMapper.addConverter(localDateStringConverter, LocalDate.class, String.class);
+
+        // Provider and converters for LocalDateTime
+        Provider<LocalDateTime> localDateTimeProvider = request -> LocalDateTime.now();
+        Converter<String, LocalDateTime> stringLocalDateTimeConverter = ctx -> {
+            String source = ctx.getSource();
+            DateTimeFormatter format = DateTimeFormatter.ISO_DATE_TIME;
+            return source == null ? null : LocalDateTime.parse(source, format);
+
+        };
+
+        Converter<LocalDateTime, String> localDateTimeStringConverter = ctx -> ctx.getSource()
+                .format(DateTimeFormatter.ISO_DATE_TIME);
+
+        modelMapper.createTypeMap(String.class, LocalDateTime.class).setProvider(localDateTimeProvider);
+        modelMapper.addConverter(stringLocalDateTimeConverter, String.class, LocalDateTime.class);
+        modelMapper.addConverter(localDateTimeStringConverter, LocalDateTime.class, String.class);
+
+        return modelMapper;
+    }
+
+}
