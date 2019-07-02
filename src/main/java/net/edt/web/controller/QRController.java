ee3107016248b3d4dedb5babecf8fd4cea0e5a32
Dophin2009 <21zhaoe@gmail.com>
2019-07-01 23:28:23 -0400

    Generate QR from SignInSession id

--- src/main/java/net/edt/web/controller/QRController.java
+++ src/main/java/net/edt/web/controller/QRController.java
@@ -1,23 +1,38 @@
 package net.edt.web.controller;
 
-import net.edt.persistence.domain.Meeting;
+import net.edt.persistence.domain.SignInSession;
+import net.edt.persistence.repository.SignInSessionRepository;
+import net.edt.web.exception.EntityNotFoundException;
 import net.edt.web.service.QRService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/admin/qrcode")
+@RequestMapping("/admin/qr")
 public class QRController {
 
     @Autowired
     private QRService qrService;
 
-    @GetMapping(produces = MediaType.IMAGE_PNG_VALUE)
-    public ResponseEntity<byte[]> produceQRCode(@RequestBody Meeting meeting) {
-        byte[] qr = qrService.generateQRCode(meeting.getDate().toString());
+    @Autowired
+    private SignInSessionRepository signInSessionRepository;
+
+    @GetMapping(value = "/{session_id}", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> produceQRCode(@PathVariable(value = "session_id") String sessionId) {
+        Optional<SignInSession> foundSession = signInSessionRepository.findById(sessionId);
+        if (!foundSession.isPresent()) {
+            throw new EntityNotFoundException("SignInSession with id '" + sessionId + "' not found");
+        }
+
+        byte[] qr = qrService.generateQRCode(sessionId);
         return new ResponseEntity<>(qr, HttpStatus.OK);
     }
 
